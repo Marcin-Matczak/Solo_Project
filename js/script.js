027ecd1042e1cb1d65f9ff4614909afedadc0ed8
Marcin-Matczak <marcorn.88@gmail.com>
2021-06-06 21:11:41 +0200

    Works done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function toggleMenu(visible) {
-    document.querySelector('.menu').classList.toggle('show', visible)
+  document.querySelector('.menu').classList.toggle('show', visible);
 }
 
 document.querySelector('.hamburger').addEventListener('click', function (e) {
-    e.preventDefault();
-    toggleMenu()
+  e.preventDefault();
+  toggleMenu();
 });
